e25fa34a9945df6de440504ece37ba19ef4707c0
HP <terlin@mail.ru>
2022-08-11 00:35:22 +0300

    start_10_08_2022

---
+++ django4_project/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def blog_index(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog_index.html', context)
